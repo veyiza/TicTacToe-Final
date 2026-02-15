ef1e33416837fee975e8b98a2663bb3c91a44fb7
veyiza <islurwhenifartt@gmail.com>
2026-02-15 01:30:31 -0800

    Buttons

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-console.log("Tic Tac Toe loaded!");
+const boardEl = document.getElementById("board");
+
+for (let i = 0; i < 9; i++) {
+  const btn = document.createElement("button");
+  btn.className = "cell";
+  btn.dataset.index = i;
+  boardEl.appendChild(btn);
+}
+
 
